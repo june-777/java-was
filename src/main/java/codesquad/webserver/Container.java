dad7e4a3758206fe643c28db65940eaf9692d064
june-777 <wlwhswnsrl96@gmail.com>
2024-07-08 16:27:30 +0900

    feat: DI 컨테이너 구현

---
+++ src/main/java/codesquad/webserver/Container.java
@@ -0,0 +1,50 @@
+package codesquad.webserver;
+
+import codesquad.servlet.HandlerMapper;
+import codesquad.servlet.MappingMediaTypeFileExtensionResolver;
+import codesquad.servlet.StaticResourceReader;
+import codesquad.servlet.handler.HttpRequestHandler;
+import codesquad.servlet.handler.StaticResourceHandler;
+import codesquad.utils.time.CurrentZonedDateTimeGenerator;
+import codesquad.utils.time.ZonedDateTimeGenerator;
+import codesquad.webserver.http.HttpRequestParser;
+
+public class Container {
+
+    public Connector connector() {
+        return new Connector(httpProcessor());
+    }
+
+    private HttpProcessor httpProcessor() {
+        return new HttpProcessor(httpRequestParser(), httpRequestHandler());
+    }
+
+    private HttpRequestParser httpRequestParser() {
+        return new HttpRequestParser();
+    }
+
+    private HttpRequestHandler httpRequestHandler() {
+        return new HttpRequestHandler(handlerMapper(), staticResourceHandler(), zonedDateTimeGenerator());
+    }
+
+    private HandlerMapper handlerMapper() {
+        return new HandlerMapper();
+    }
+
+    private StaticResourceHandler staticResourceHandler() {
+        return new StaticResourceHandler(staticResourceReader(), mappingMediaTypeFileExtensionResolver());
+    }
+
+    private StaticResourceReader staticResourceReader() {
+        return new StaticResourceReader();
+    }
+
+    private MappingMediaTypeFileExtensionResolver mappingMediaTypeFileExtensionResolver() {
+        return new MappingMediaTypeFileExtensionResolver();
+    }
+
+    private ZonedDateTimeGenerator zonedDateTimeGenerator() {
+        return new CurrentZonedDateTimeGenerator();
+    }
+
+}
