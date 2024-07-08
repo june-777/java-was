d4e446f6bea232be840dfd5d7259e24469c4c0b5
june-777 <wlwhswnsrl96@gmail.com>
2024-07-08 16:28:50 +0900

    refactor: 참조관계 설정을 Container에게 위임

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,10 +1,12 @@
 package codesquad;
 
 import codesquad.webserver.Connector;
+import codesquad.webserver.Container;
 
 public class Main {
     public static void main(String[] args) {
-        Connector connector = new Connector();
+        Container container = new Container();
+        Connector connector = container.connector();
         connector.start();
     }
 }
