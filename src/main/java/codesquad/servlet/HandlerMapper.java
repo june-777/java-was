9aaee417ec52e25d798d2313779bd979b2e1626c
june-777 <wlwhswnsrl96@gmail.com>
2024-07-09 16:33:38 +0900

    feat: HandlerMapper 식별자에 HttpMethod 추가

--- src/main/java/codesquad/servlet/HandlerMapper.java
+++ src/main/java/codesquad/servlet/HandlerMapper.java
@@ -2,20 +2,25 @@ package codesquad.servlet;
 
 import codesquad.servlet.handler.Handler;
 import codesquad.servlet.handler.UserRegistrationHandler;
+import codesquad.webserver.http.HttpMethod;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class HandlerMapper {
 
-    private final Map<String, Handler> handlers = new ConcurrentHashMap<>();
+    private final Map<HttpMethod, Map<String, Handler>> handlers;
 
     public HandlerMapper() {
-        handlers.put("/create", new UserRegistrationHandler());
+        handlers = new HashMap<>();
+        for (HttpMethod httpMethod : HttpMethod.values()) {
+            handlers.put(httpMethod, new HashMap<>());
+        }
+        handlers.get(HttpMethod.POST).put("/create", new UserRegistrationHandler());
     }
 
-    public Optional<Handler> findHandler(String path) {
-        return Optional.ofNullable(handlers.get(path));
+    public Optional<Handler> findBy(HttpMethod httpMethod, String path) {
+        return Optional.ofNullable(handlers.get(httpMethod).get(path));
     }
 
 }
