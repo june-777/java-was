478f0ce69f047284040161f2926bcbcc41f8c36e
june-777 <wlwhswnsrl96@gmail.com>
2024-07-16 15:57:25 +0900

    feat: 테스트용 UserFixture 추가

--- src/test/java/codesquad/servlet/fixture/UserFixture.java
+++ src/test/java/codesquad/servlet/fixture/UserFixture.java
@@ -8,4 +8,8 @@ public class UserFixture {
         return new User("아이디1", "비밀번호1", "이름1", "email1@woowah.com");
     }
 
+    public static User createUser2() {
+        return new User("아이디2", "비밀번호2", "이름2", "email2@woowah.com");
+    }
+
 }
