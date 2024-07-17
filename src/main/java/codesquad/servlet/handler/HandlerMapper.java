5a77952d3dbfc719a4811ec0bdf637e6c38bb279
june-777 <wlwhswnsrl96@gmail.com>
2024-07-17 15:24:02 +0900

    feat: 게시글 작성 핸들러를 핸들러 매퍼에 추가

--- src/main/java/codesquad/servlet/handler/HandlerMapper.java
+++ src/main/java/codesquad/servlet/handler/HandlerMapper.java
@@ -1,8 +1,18 @@
 package codesquad.servlet.handler;
 
-import codesquad.domain.InMemoryUserStorage;
+import static codesquad.servlet.database.property.DataSource.JDBC_URL;
+import static codesquad.servlet.database.property.DataSource.PASSWORD;
+import static codesquad.servlet.database.property.DataSource.USER_NAME;
+
+import codesquad.domain.ArticleStorage;
+import codesquad.domain.UserStorage;
 import codesquad.servlet.SessionStorage;
+import codesquad.servlet.database.connection.ArticleDao;
+import codesquad.servlet.database.connection.DatabaseConnector;
+import codesquad.servlet.database.connection.UserDao;
+import codesquad.servlet.database.property.DataSourceProvider;
 import codesquad.servlet.handler.api.AllUserInfoHandler;
+import codesquad.servlet.handler.api.ArticleWriteHandler;
 import codesquad.servlet.handler.api.UserInfoHandler;
 import codesquad.servlet.handler.api.UserLoginHandler;
 import codesquad.servlet.handler.api.UserLogoutHandler;
@@ -22,15 +32,19 @@ public class HandlerMapper {
             handlers.put(httpMethod, new HashMap<>());
         }
 
-        InMemoryUserStorage inMemoryUserStorage = InMemoryUserStorage.getInstance();
+        DataSourceProvider dataSourceProvider =
+                new DataSourceProvider(JDBC_URL.getProperty(), USER_NAME.getProperty(), PASSWORD.getProperty());
+        DatabaseConnector databaseConnector = new DatabaseConnector(dataSourceProvider);
+        UserStorage userStorage = new UserDao(databaseConnector);
         SessionStorage sessionStorage = SessionStorage.getInstance();
+        ArticleStorage articleStorage = new ArticleDao(databaseConnector);
 
         handlers.get(HttpMethod.POST)
-                .put("/user/create", new UserRegistrationHandler(inMemoryUserStorage));
+                .put("/user/create", new UserRegistrationHandler(userStorage));
 
         handlers.get(HttpMethod.POST)
                 .put("/user/login", new UserLoginHandler(
-                        inMemoryUserStorage, sessionStorage));
+                        userStorage, sessionStorage));
 
         handlers.get(HttpMethod.GET)
                 .put("/user/logout", new UserLogoutHandler(sessionStorage));
@@ -39,7 +53,10 @@ public class HandlerMapper {
                 .put("/api/user/info", new UserInfoHandler(sessionStorage));
 
         handlers.get(HttpMethod.GET)
-                .put("/api/user/list", new AllUserInfoHandler(inMemoryUserStorage, sessionStorage));
+                .put("/api/user/list", new AllUserInfoHandler(userStorage));
+
+        handlers.get(HttpMethod.POST)
+                .put("/article", new ArticleWriteHandler(sessionStorage, articleStorage));
     }
 
     public Optional<Handler> findBy(HttpMethod httpMethod, String path) {
