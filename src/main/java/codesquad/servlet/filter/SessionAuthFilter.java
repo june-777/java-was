fae427940da7de140a538afeef139971638f6083
june-777 <wlwhswnsrl96@gmail.com>
2024-07-18 15:49:17 +0900

    feat: 인증이 필요한 요청에 게시글 작성 정적 페이지 요청 추가

--- src/main/java/codesquad/servlet/filter/SessionAuthFilter.java
+++ src/main/java/codesquad/servlet/filter/SessionAuthFilter.java
@@ -14,7 +14,7 @@ public class SessionAuthFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(SessionAuthFilter.class);
     private static final List<String> AUTH_URI = List.of("/user/list", "/user/logout", "/api/user/info",
-            "/api/user/list", "/article/write.html");
+            "/api/user/list", "/article/write.html", "/comment");
     private final SessionStorage sessionStorage;
 
     public SessionAuthFilter(SessionStorage sessionStorage) {
